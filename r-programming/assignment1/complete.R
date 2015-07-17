84fbf9ce45da6c222cde24a6a1f0b29cb86509ca
Robert Perez <robertjasonperez@gmail.com>
2015-07-17 00:02:24 -0500

    Part 2 of hw 1 complete
    data frame created

---
+++ r-programming/assignment1/complete.R
@@ -0,0 +1,21 @@
+complete <- function(directory,id = 1:332){
+  files <- constructFileList(directory,id)
+  
+  counts <- c()
+  for(file in files){
+    dataTable <- read.csv(file)
+    
+    counts<- c(counts,length(dataTable$ID[(!is.na(dataTable$nitrate)) & (!is.na(dataTable$sulfate))]))
+  }
+  data.frame(id = id,nobs = counts)
+}
+
+
+constructFileList <- function(directory,id){
+  files <- dir(directory)
+  fileList<-c()
+  for(sensor in id){
+    fileList <- c(fileList,paste(directory,"\\",files[sensor],sep = ""))
+  }
+  fileList
+}
