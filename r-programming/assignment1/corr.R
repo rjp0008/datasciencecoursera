8a135f00694f31912e3bd9ce07645b7d6bd92e54
Robert Perez <robertjasonperez@gmail.com>
2015-07-17 23:49:26 -0500

    Part 3
    Part 3 of HW 1 complete

---
+++ r-programming/assignment1/corr.R
@@ -0,0 +1,25 @@
+corr <- function(directory,threshold = 0){
+  files <- constructFileListNoId(directory)
+  
+  cors <- integer(0)
+  for(file in files){
+    dataTable <- read.csv(file)
+    
+    nitrate <- (dataTable$nitrate[(!is.na(dataTable$nitrate)) & (!is.na(dataTable$sulfate))])
+    if(length((nitrate))>threshold){
+      sulfateVector <-(dataTable$sulfate[(!is.na(dataTable$nitrate)) & (!is.na(dataTable$sulfate))])
+      cors <- c(cors,cor(nitrate,sulfateVector))
+    }  
+  }
+  cors
+}
+
+
+constructFileListNoId <- function(directory){
+  files <- dir(directory)
+  fileList<-c()
+  for(sensor in files){
+    fileList <- c(fileList,paste(directory,"\\",sensor,sep = ""))
+  }
+  fileList
+}
