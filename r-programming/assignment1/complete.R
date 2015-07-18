4b819171575e4327c1fcf32baaab8eb7d18ca7de
Robert Perez <robertjasonperez@gmail.com>
2015-07-18 12:57:58 -0500

    Formatting
    Applied new formatting to source files, no code changes.

--- r-programming/assignment1/complete.R
+++ r-programming/assignment1/complete.R
@@ -1,21 +1,23 @@
-complete <- function(directory,id = 1:332){
-  files <- constructFileList(directory,id)
-  
-  counts <- c()
-  for(file in files){
-    dataTable <- read.csv(file)
-    
-    counts<- c(counts,length(dataTable$ID[(!is.na(dataTable$nitrate)) & (!is.na(dataTable$sulfate))]))
-  }
-  data.frame(id = id,nobs = counts)
+complete <- function(directory,id = 1:332) {
+    files <- constructFileList(directory,id)
+
+    counts <- c()
+    for (file in files) {
+        dataTable <- read.csv(file)
+
+        counts <-
+            c(counts,length(dataTable$ID[(!is.na(dataTable$nitrate)) &
+                                             (!is.na(dataTable$sulfate))]))
+    }
+    data.frame(id = id,nobs = counts)
 }
 
 
-constructFileList <- function(directory,id){
-  files <- dir(directory)
-  fileList<-c()
-  for(sensor in id){
-    fileList <- c(fileList,paste(directory,"\\",files[sensor],sep = ""))
-  }
-  fileList
+constructFileList <- function(directory,id) {
+    files <- dir(directory)
+    fileList <- c()
+    for (sensor in id) {
+        fileList <- c(fileList,paste(directory,"\\",files[sensor],sep = ""))
+    }
+    fileList
 }
--- r-programming/assignment1/pollutantmean.R
+++ r-programming/assignment1/pollutantmean.R
@@ -1,37 +1,37 @@
 ##parses through id files in directory for pollutant specified
 ##and calculates the mean excluding missing values
-pollutantmean <- function(directory, pollutant, id = 1:332){
-  ## directory is where the files are located
-  dataVector <- NULL
-  for(file in constructFileListNoDirectory(directory,id)){
-    
-    dataTable <- read.csv(paste(directory,"\\",file,sep = ""))
-    
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    ## directory is where the files are located
+    dataVector <- NULL
+    for (file in constructFileListNoDirectory(directory,id)) {
+        dataTable <- read.csv(paste(directory,"\\",file,sep = ""))
 
-    #pollutant is sulfate or nitrate
-    removeNas<-NULL
-    if(pollutant == "sulfate"){
-      removeNas <-  dataTable$sulfate
+
+        #pollutant is sulfate or nitrate
+        removeNas <- NULL
+        if (pollutant == "sulfate") {
+            removeNas <-  dataTable$sulfate
+        }
+        else{
+            removeNas <- dataTable$nitrate
+
+        }
+        removeNas <- removeNas[!is.na(removeNas)]
+        dataVector <- c(dataVector,removeNas)
     }
-    else{
-      removeNas <- dataTable$nitrate
-    }
-    removeNas <- removeNas[!is.na(removeNas)]
-    dataVector <- c(dataVector,removeNas)
-  }
-  
-  
-  mean(dataVector[!is.na(dataVector)])
-  ##id is a vector of the sensors to check
-  
+
+
+    mean(dataVector[!is.na(dataVector)])
+    ##id is a vector of the sensors to check
+
 }
 
 
-constructFileListNoDirectory <- function(directory,id){
-  files <- dir(directory)
-  fileList<-c()
-  for(sensor in id){
-    fileList <- c(fileList,files[sensor])
-  }
-  fileList
+constructFileListNoDirectory <- function(directory,id) {
+    files <- dir(directory)
+    fileList <- c()
+    for (sensor in id) {
+        fileList <- c(fileList,files[sensor])
+    }
+    fileList
 }
